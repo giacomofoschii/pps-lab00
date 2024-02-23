8a36cd16648836dce73eb3655e46a46e3ce66abb
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2024-02-23 17:16:15 +0100

    :sparkles: prefer complete names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,7 @@ import utils.Logger;
 
 public class Main {
     public static void main(final String[] args){
-        final Logger l = new Logger();
-        Person p = new Person("Bob", 33);
-        Logger.logPrintString("Hello " + p);
+        Person person = new Person("Gianlu", 28);
+        Logger.logPrintString("Hello " + person);
     }
 }
